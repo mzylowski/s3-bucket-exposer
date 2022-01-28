d5e7cd8c1131e48bc7c427d29912fbd6165319e4
Michał Żyłowski <michal@zylowski.net>
2022-01-28 19:46:47 +0100

    Downloading files is working.
    Signed-off-by: Michał Żyłowski <michal@zylowski.net>

--- providers/base.py
+++ providers/base.py
@@ -5,7 +5,8 @@ from objects.s3_object import S3Object
 class BaseProvider(object):
     def __init__(self):
         self.client = self.get_client()
-        self.is_bucket_allowed = lambda x: x in Cm.get_exposer_allowed_buckets()
+        self.all_buckets_allowed = lambda: Cm.get_exposer_allowed_buckets() == consts.ALL_BUCKETS_ALLOWED
+        self.is_bucket_allowed = lambda x: self.all_buckets_allowed or x in Cm.get_exposer_allowed_buckets()
 
     def get_client(self):
         pass
@@ -16,12 +17,17 @@ class BaseProvider(object):
         return self._intersect_allowed_buckets(s3_buckets)
 
     def list_of_objects(self, bucket_name):
-        accessible_buckets = self.list_of_buckets()
-        if bucket_name not in accessible_buckets:
+        if not self.is_bucket_allowed(bucket_name):
             return None
         response = self.client.list_objects(Bucket=bucket_name).get("Contents", [])
         return [S3Object(name=obj['Key'], date=obj['LastModified'], size=obj['Size']) for obj in response]
 
+    def generate_download_url(self, bucket, key):
+        if self.is_bucket_allowed(bucket):
+            return self.client.generate_presigned_url('get_object',
+                                                      Params={'Bucket': bucket, 'Key': key},
+                                                      ExpiresIn=15)
+        return None
+
     def _intersect_allowed_buckets(self, s3_buckets):
-        return s3_buckets if Cm.get_exposer_allowed_buckets() == consts.ALL_BUCKETS_ALLOWED else \
-            [bucket for bucket in s3_buckets if self.is_bucket_allowed(bucket)]
+        return s3_buckets if self.all_buckets_allowed else [b for b in s3_buckets if self.is_bucket_allowed(b)]
