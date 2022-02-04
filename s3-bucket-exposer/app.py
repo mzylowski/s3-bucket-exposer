2f0035d4674ef66bc99a7f8a319690c156d28fff
Michal Zylowski <michal@zylowski.net>
2022-02-04 16:43:20 +0100

    Add HTML templates for HTML exposer
    Signed-off-by: Michal Zylowski <michal@zylowski.net>

--- s3-bucket-exposer/app.py
+++ s3-bucket-exposer/app.py
@@ -10,21 +10,20 @@ provider, exposer = helpers.initialize()
 
 @application.route("/")
 def index():
-    if type(exposer) == BaseExposer:
-        return ""
-    return exposer.expose_list_of_buckets(provider.list_of_buckets())
+    return "" if type(exposer) == BaseExposer else exposer.expose_list_of_buckets(provider.list_of_buckets())
 
 
 @application.route("/bucket/<bucket_name>")
 def file_list(bucket_name):
-    if type(exposer) == BaseExposer:
-        return ""
-    return exposer.expose_list_of_objects(provider.list_of_objects(bucket_name))
+    return "" if type(exposer) == BaseExposer else \
+        exposer.expose_list_of_objects(
+            bucket_name,
+            provider.list_of_objects(bucket_name))
 
 
-@application.route("/download/<bucket_name>/<file_name>")
-def download_file(bucket_name, file_name):
-    return exposer.redirect(provider.generate_download_url(bucket_name, file_name))
+@application.route("/download/<bucket_name>/<path:rest>")
+def download_file(bucket_name, rest):
+    return exposer.redirect(provider.generate_download_url(bucket_name, rest))
 
 
 if __name__ == "__main__":
--- s3-bucket-exposer/exposers/html.py
+++ s3-bucket-exposer/exposers/html.py
@@ -1,13 +1,18 @@
+from flask import render_template
+
+from configuration import consts
 from exposers.base import BaseExposer
 from objects.s3_object import S3Object
 
 
 class HTMLExposer(BaseExposer):
     def expose_list_of_buckets(self, buckets):
-        return "</br>".join(buckets)
+        return render_template("bucket_list.html",
+                               buckets=buckets,
+                               product_version=consts.APP_FULL_NAME)
 
-    def expose_list_of_objects(self, objects: [S3Object]):
-        code = ""
-        for obj in objects:
-            code += f"{obj.name}</br>"
-        return code
+    def expose_list_of_objects(self, bucket, objects: [S3Object]):
+        return render_template("object_list.html",
+                               bucket=bucket,
+                               objects=objects,
+                               product_version=consts.APP_FULL_NAME)
