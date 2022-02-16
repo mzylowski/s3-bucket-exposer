a357b35204f4c6bc2e16466914e7a1c517ebdf0e
Michał Żyłowski <michal@zylowski.net>
2022-02-16 02:03:04 +0100

    Introduce S3BE test case class
    Signed-off-by: Michał Żyłowski <michal@zylowski.net>

---
+++ tests/suite_helpers/s3be_test_case.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from suite_helpers.functions import initialize_container
+
+
+class S3BucketExposerTestCase(TestCase):
+    original_failureException = TestCase.failureException
+
+    @property
+    def failureException(self):
+        self.container_id_for_logs.append(self.docker)
+        return self.original_failureException
+
+    @classmethod
+    def setUpClass(cls):
+        cls.container_id_for_logs = []
+        cls.docker = initialize_container()
+
+    @classmethod
+    def tearDownClass(cls):
+        for container in set(cls.container_id_for_logs):
+            print(f">>>>>>>>>>>> Docker logs for {container.container} ({container.container_ip})")
+            # (TODO) print logs from docker container
+            print("<<<<<<<<<<<<")
+        cls.docker.stop_delete_container()
